3a84a128b5df0673835de7dee5133492a69c9336
Glitch (hello-webpage) <none>
2020-12-21 15:24:24 +0000

    🍰💿 Checkpoint ./script.js:24042303/2714 ./index.html:24042303/18

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-var 
+var work = "1";
+var console1 = document.getElementById("console1");
+var console2 = document.getElementById("console2");
+var console3 = document.getElementById("console3");
+
+
+document.getElementById("console1").;
+  
