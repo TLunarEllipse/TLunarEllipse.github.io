ae195d0da151824896e8ebf7ef0f65e35d14d900
Glitch (hello-webpage) <none>
2020-12-21 15:42:28 +0000

    👂📡 Checkpoint ./script.js:24042303/3634

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 var work = 1;
-var system;
+var system = true;
 var console1 = document.getElementById("console1");
 var console2 = document.getElementById("console2");
 var console3 = document.getElementById("console3");
-
-if (system) {
-  document.getElementById("console1").innerHTML = "Program statistics:" + "";
-}
 
 function run() {
   lexer();
 
   //define whether the code is right or wrong:
+
+  //right:
   if ((work = 1)) {
     document.getElementById("console1").innerHTML = "Program statistics:" + "";
     document.getElementById("console2").innerHTML = "Console Output:" + "";
     execute();
   }
-
+  //wrong:
   if ((work = 0)) {
     document.getElementById("console1").innerHTML = "Program statistics:" + "";
     document.getElementById("console2").innerHTML = "Console Output:" + "";
