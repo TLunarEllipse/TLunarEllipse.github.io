6cdb8060f22bc8658f3522b929521fc60ef1ec9c
Glitch (hello-webpage) <none>
2020-12-21 15:35:33 +0000

    🎈🏰 Checkpoint ./script.js:24042303/2343

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-var work = "1";
+var work = 1;
+var system;
 var console1 = document.getElementById("console1");
 var console2 = document.getElementById("console2");
 var console3 = document.getElementById("console3");
 
+if (system) {
+  document.getElementById("console1").innerHTML = "Program statistics:" + "";
+}
 
-document.getElementById("console1").;
-  
+function run() {
+  lexer();
+
+  //define whether the code is right or wrong:
+  if ((work = 1)) {
+    document.getElementById("console1").innerHTML = "Program statistics:" + "";
+    document.getElementById("console2").innerHTML = "Console Output:" + "";
+    execute();
+  }
+
+  if ((work = 0)) {
+    document.getElementById("console1").innerHTML = "Program statistics:" + "";
+    document.getElementById("console2").innerHTML = "Console Output:" + "";
+    alert("[i]error");
+  }
+}
+
+//execute code:
+function execute() {}
+
+//lexical analysis:
+function lexer() {}
