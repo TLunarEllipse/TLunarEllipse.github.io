34d464f97c90bfa9faab2891092d36157bd21491
Glitch (hello-webpage) <none>
2020-12-22 16:04:34 +0000

    🎬🐈 Checkpoint ./script.js:24042303/4713

--- script.js
+++ script.js
@@ -73,9 +73,11 @@ function execute() {}
 //lexical analysis:
 function lexer() {
   code1 = document.getElementById("console3").value;
+
+  code1 = code1.split(" ");
   
-  code1 = code1.split;
-  
+  code1 = code1.repalce(",","bruh");
+
   alert(code1);
   execute();
 }
