68221c6ab2d36617c562cf2691371c25cf8a1b90
Glitch (test-30807060-69679) <none>
2020-12-24 19:01:44 +0000

    📰☕️ Checkpoint ./script.js:24042303/17908

--- script.js
+++ script.js
@@ -9,8 +9,45 @@ var fulldate;
 var erro = "No Errors Found";
 var code1;
 var code2;
+var bl = '
+var FUNCTIONS = ["print", "var", "or", "if", "else", "loop", "and", "not"];
 
-//specifyions:
+var CHARACTERS = [
+  "+",
+  "-",
+  "*",
+  "/",
+  "^",
+  "(",
+  ")",
+  "[",
+  "]",
+  ",",
+  ";",
+  "=",
+  "<",
+  ">",
+  "'",
+  '"'
+];
+var CH_CHARACTERS = [
+  "CH_PLU",
+  "CH_MIN",
+  "CH_MUL",
+  "CH_DIV",
+  "CH_POW",
+  "CH_LPA",
+  "CH_RPA",
+  "CH_LSQ",
+  "CH_RSQ",
+  "CH_COM",
+  "CH_SEM",
+  "CH_EQU",
+  "CH_LES",
+  "CH_GRE",
+  "CH_SAS",
+  "CH_ASP"
+];
 
 //run code:
 
@@ -55,6 +92,11 @@ function lexer() {
   ///start commas system 1:
   while (code1.includes(" ")) {
     code1 = code1.replace(" ", "√•√");
+  }
+  while (code1.includes("")) {
+
+    code1 = code1.replace("", "√•√");
+
   }
   ///end commas system 1
 
@@ -109,7 +151,7 @@ function lexer() {
   }
 
   while (code1.includes("<")) {
-    code1 = code1.replace("", "CH_LES");
+    code1 = code1.replace("<", "CH_LES");
   }
 
   while (code1.includes(">")) {
@@ -125,7 +167,30 @@ function lexer() {
   }
 
   //FUNCTIONS:
-
+  while (code1.includes("print")) {
+    code1 = code1.replace("print", "FU_PRINT");
+  }
+  while (code1.includes("var")) {
+    code1 = code1.replace("var", "FU_VAR");
+  }
+  while (code1.includes("or")) {
+    code1 = code1.replace("or", "FU_OR");
+  }
+  while (code1.includes("if")) {
+    code1 = code1.replace("if", "FU_IF");
+  }
+  while (code1.includes("else")) {
+    code1 = code1.replace("else", "FU_ELSE");
+  }
+  while (code1.includes("loop")) {
+    code1 = code1.replace("loop", "FU_LOOP");
+  }
+  while (code1.includes("and")) {
+    code1 = code1.replace("and", "FU_AND");
+  }
+  while (code1.includes("not")) {
+    code1 = code1.replace("not", "FU_NOT");
+  }
   //STRINGS:
 
   ///start commas system 2:
