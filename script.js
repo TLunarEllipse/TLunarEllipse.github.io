19743d64603da7445fa17e98657488082dbd5abc
Glitch (test-30807060-69679) <none>
2021-01-06 21:18:15 +0000

    Rewound to commit 3f834be22053548a5f0dc1709197edd39bdcf23c + 0 edits

--- script.js
+++ script.js
@@ -392,48 +392,7 @@ function spacing2() {
 }
 //syntax analisys:
 function phaser() {
-  Expression = head:Term tail:(_ ("+" / "-") _ Term)* {
-
-      return tail.reduce(function(result, element) {
-
-        if (element[1] === "+") { return result + element[3]; }
-
-        if (element[1] === "-") { return result - element[3]; }
-
-      }, head);
-
-    }
-
-Term
-
-  = head:Factor tail:(_ ("*" / "/") _ Factor)* {
-
-      return tail.reduce(function(result, element) {
-
-        if (element[1] === "*") { return result * element[3]; }
-
-        if (element[1] === "/") { return result / element[3]; }
-
-      }, head);
-
-    }
-
-Factor
-
-  = "(" _ expr:Expression _ ")" { return expr; }
-
-  / Integer
-
-Integer "integer"
-
-  = _ [0-9]+ { return parseInt(text(), 10); }
-
-_ "whitespace"
-
-  = [ \t\n\r]*
-  
   code2 = "(" + codeS + ")";
-  
   if (debug == 1) {
     alert(code2);
   }
