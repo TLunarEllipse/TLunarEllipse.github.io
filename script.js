f511b9baf51ccfa877d75b535a1a070ee2fb56f5
Glitch (test-30807060-69679) <none>
2021-01-08 04:07:22 +0000

    Rewound to commit d3be9b963d415e1a9c001631a48f9dc1bf2ed3e9 + 0 edits

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var console1 = document.getElementById("console1");
 var console2 = document.getElementById("console2");
 var console3 = document.getElementById("console3");
 var fulldate;
-var erro = "No erros founded";
+var erro = "teste teste teste teste teste teste teste teste teste teste teste";
 var code;
 var code0;
 var code1;
