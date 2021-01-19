2c6c24c3485f8500d45866ae3e561c8a1b1591a4
Glitch (test-30807060pi) <none>
2021-01-19 19:03:28 +0000

    🎭🛋 Checkpoint ./script.js:24042303/50

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var ltlcode1;
 var ltlcodeS;
 var ltlcode2;
 var ltlcode3;
-var ltllink = "https://TLunarEllipse.github.io/";
+var ltllink = "https://test-30807060pi.glitch.me/";
 var ltldebug = 0;
 var ltltokenspacing = 1;
 var ltlmaxtimes;
